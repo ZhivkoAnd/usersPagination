7bd341229cbc50a600e2cf3446d6d1f07f104d42
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-03-05 19:38:40 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
-// function App() {
-//   const [nextPage, setNextPage] = useState(1);
-//   const [randomUserData, setRandomUserData] = useState("");
-
-//   const fetchNextUser = async () => {
-//     const result = await fetch(`https://randomuser.me/api?page=${nextPage}`);
-//     const data = await result.json();
-
-//     const newData = data.results;
-
-//     setData(newData);
-//     setNextPage(data.info.page + 1);
-//   };
-
-//   useEffect(() => {
-//     fetchNextUser();
-//   }, []);
-
 const App = () => {
   const [userInfos, setUserInfos] = useState("");
   const [nextPageNumber, setNextPageNumber] = useState(1);
 
   const fetchNextUser = () => {
     const fetchRandomData = async (pageNumber) => {
-      const data = await fetch(`https://randomuser.me/api?page=${pageNumber}`);
-      const randomData = await data.json();
-      const newUserInfos = [...userInfos, ...randomData.results];
+      const result = await fetch(
+        `https://randomuser.me/api?page=${pageNumber}`
+      );
+      const data = await result.json();
+      // userInfos is empty at this point
+      console.log(userInfos);
+      console.log(data.results);
+      const newUserInfos = [...userInfos, ...data.results];
       setUserInfos(newUserInfos);
-      console.log(randomData);
-      setNextPageNumber(randomData.info.page + 1);
+      setNextPageNumber(data.info.page + 1);
     };
-    fetchRandomData();
+    fetchRandomData(nextPageNumber);
   };
 
   useEffect(() => {
@@ -45,7 +31,7 @@ const App = () => {
         {userInfos &&
           userInfos.map((user) => {
             return (
-              <div key={user.id}>
+              <div key={user.email}>
                 <div>{user.gender}</div>
                 <img src={user.picture?.thumbnail} alt="hey" />
               </div>
